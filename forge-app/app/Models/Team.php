3903a30933432ad6a9141d40374a2358122074c4
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-08-02 15:57:54 -0400

    Update Team.php

--- forge-app/app/Models/Team.php
+++ forge-app/app/Models/Team.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\PermissionGroup;
+use App\Traits\HasPermissions;
 
 class Team extends Model
 {
     use HasFactory;
+    use HasPermissions;
+
+    public function permissionGroups()
+    {
+        return $this->belongsToMany(PermissionGroup::class, 'team_permission_group');
+    }
 }
