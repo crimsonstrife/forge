631570722166a78f1371814ddb068d2e124e6df6
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-08-02 15:57:51 -0400

    Update Role.php

--- forge-app/app/Models/Role.php
+++ forge-app/app/Models/Role.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\PermissionGroup;
+use App\Traits\HasPermissions;
 
 class Role extends Model
 {
     use HasFactory;
+    use HasPermissions;
+
+    // In Role model
+    public function permissionGroups()
+    {
+        return $this->belongsToMany(PermissionGroup::class, 'role_permission_group');
+    }
 }
