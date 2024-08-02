49b4718e8731c7714424a898514388b54a34ab17
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-08-02 14:58:29 -0400

    Update api.php

--- forge-app/routes/api.php
+++ forge-app/routes/api.php
@@ -2,14 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\Project;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 /**
- * Setup API endpoints for user data, project issues/tasks, repository details, and more.
+ * Setup API endpoints for user data, project issues, repository details, and more.
  */
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware('auth:api')->get('/projects', function (Request $request, Project $project) {
+    return $project->getProjects();
+});
+Route::middleware('auth:api')->get('/projects/{project}', function (Request $request, Project $project) {
+    return $project;
+});
+Route::middleware('auth:api')->get('/projects/{project}/issues', function (Request $request, Project $project) {
+    return $project->issues;
 });
