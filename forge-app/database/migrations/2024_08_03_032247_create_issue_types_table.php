b25218e591197ea54d0121f5c6545ff1cd45942f
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-03 00:45:05 -0400

    Create 2024_08_03_032247_create_issue_types_table.php

---
+++ forge-app/database/migrations/2024_08_03_032247_create_issue_types_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('issue_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('icon');
+            $table->string('color')->default('#cecece');
+            $table->string('description')->nullable();
+            $table->boolean('is_default')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('issue_types');
+    }
+};
