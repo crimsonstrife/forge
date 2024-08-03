ac9466a271b7aea60e9d54a6b137fc65e2a32afc
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-03 00:47:53 -0400

    Create 2024_08_03_035557_add_project_to_issue_statuses.php

---
+++ forge-app/database/migrations/2024_08_03_035557_add_project_to_issue_statuses.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('issue_statuses', function (Blueprint $table) {
+            $table->bigInteger('project_id')->unsigned();
+        });
+
+        Schema::table('issue_statuses', function (Blueprint $table) {
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('issue_statuses', function (Blueprint $table) {
+            $table->dropForeign(['project_id']);
+            $table->dropColumn('project_id');
+        });
+    }
+};
