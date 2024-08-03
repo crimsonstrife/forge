c84217700cd32f4b766559bd8fb1f3b2cdbb410a
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-03 01:53:17 -0400

    Create 2024_08_03_052539_create_project_has_repository_table.php

---
+++ forge-app/database/migrations/2024_08_03_052539_create_project_has_repository_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_has_repository', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('project_id')->unsigned()->unique();
+            $table->bigInteger('repository_id')->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::table('project_has_repository', function (Blueprint $table) {
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('repository_id')->references('id')->on('repository');
+        });
+
+        //set unique constraint
+        Schema::table('project_has_repository', function (Blueprint $table) {
+            $table->unique(['project_id', 'repository_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_has_repository');
+    }
+};
