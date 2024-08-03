7074f0e495e871b22e50cf4a46398ec7ce1801fb
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-03 01:54:01 -0400

    Create 2024_08_03_053128_create_issue_has_comments_table.php

---
+++ forge-app/database/migrations/2024_08_03_053128_create_issue_has_comments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('issue_has_comments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('issue_id')->unsigned();
+            $table->bigInteger('comment_id')->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::table('issue_has_comments', function (Blueprint $table) {
+            $table->foreign('issue_id')->references('id')->on('issues');
+            $table->foreign('comment_id')->references('id')->on('comment');
+        });
+
+        //set unique constraint
+        Schema::table('issue_has_comments', function (Blueprint $table) {
+            $table->unique(['issue_id', 'comment_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('issue_has_comments');
+    }
+};
