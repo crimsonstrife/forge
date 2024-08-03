038d0438de12722f4bd530e58bf60be76c02ada9
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-03 16:57:00 -0400

    Update 2024_08_02_192557_create_teams_table.php

--- forge-app/database/migrations/2024_08_02_192557_create_teams_table.php
+++ forge-app/database/migrations/2024_08_02_192557_create_teams_table.php
@@ -13,7 +13,22 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('description')->nullable()->default(null);
+            $table->bigInteger('owner_user_id')->unsigned();
+            $table->softDeletes();
             $table->timestamps();
+        });
+
+        Schema::table('teams', function (Blueprint $table) {
+            $table->foreign('owner_user_id')->references('id')->on('users');
+        });
+
+        // Add a unique constraint to prevent duplicate teams
+        Schema::table('teams', function (Blueprint $table) {
+            $table->unique(['owner_user_id', 'slug', 'uuid'], 'unique_user_teams');
         });
     }
 
