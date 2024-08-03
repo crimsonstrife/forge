35d6e77fb8b821718bed388bb7edf71222b12dee
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-03 01:44:52 -0400

    Update 2024_08_03_035025_create_issue_priorities_table.php

--- forge-app/database/migrations/2024_08_03_035025_create_issue_priorities_table.php
+++ forge-app/database/migrations/2024_08_03_035025_create_issue_priorities_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('issue_priorities', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('icon')->nullable();
             $table->string('color')->default('#cecece');
             $table->boolean('is_default')->default(false);
             $table->softDeletes();
