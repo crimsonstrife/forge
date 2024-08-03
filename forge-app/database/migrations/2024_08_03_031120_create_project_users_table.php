8e85827c72151701509bba12dd5c1f4008a6b942
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-03 00:43:49 -0400

    Create 2024_08_03_031120_create_project_users_table.php

---
+++ forge-app/database/migrations/2024_08_03_031120_create_project_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_users', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('project_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('role');
+            $table->timestamps();
+        });
+
+        Schema::table('project_users', function (Blueprint $table) {
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        //set constraints
+        Schema::table('project_users', function (Blueprint $table) {
+            $table->unique(['project_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_users');
+    }
+};
