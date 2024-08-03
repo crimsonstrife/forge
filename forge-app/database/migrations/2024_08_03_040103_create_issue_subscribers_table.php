6bd36646e469fa650b2975fa6e6b2387bdcd3bbb
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-03 00:48:21 -0400

    Create 2024_08_03_040103_create_issue_subscribers_table.php

---
+++ forge-app/database/migrations/2024_08_03_040103_create_issue_subscribers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('issue_subscribers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('issue_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('issue_subscribers', function (Blueprint $table) {
+            $table->foreign('issue_id')->references('id')->on('issues');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('issue_subscribers');
+    }
+};
