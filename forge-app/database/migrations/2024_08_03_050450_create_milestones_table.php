8b46adcd8b1c3485fa11473b0bb7f30b6f116065
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-03 01:08:35 -0400

    Create 2024_08_03_050450_create_milestones_table.php

---
+++ forge-app/database/migrations/2024_08_03_050450_create_milestones_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('milestones', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->longText('description')->nullable();
+            $table->bigInteger('project_id')->unsigned();
+            $table->bigInteger('sprint_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('milestones', function (Blueprint $table) {
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('sprint_id')->references('id')->on('sprints')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('milestones');
+    }
+};
