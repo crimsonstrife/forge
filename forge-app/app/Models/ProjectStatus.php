c354efa5093d233869cb8b9baa0fa40c089b7919
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-08-06 16:00:56 -0400

    Update ProjectStatus.php

--- forge-app/app/Models/ProjectStatus.php
+++ forge-app/app/Models/ProjectStatus.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProjectStatus extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'color',
+        'is_default',
+        'description'
+    ];
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class, 'status_id', 'id');
+    }
 }
