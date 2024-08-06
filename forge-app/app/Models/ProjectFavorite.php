3725e7062291e15b3450b8f05f90972af6de5229
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-08-06 16:00:54 -0400

    Update ProjectFavorite.php

--- forge-app/app/Models/ProjectFavorite.php
+++ forge-app/app/Models/ProjectFavorite.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProjectFavorite extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'project_id',
+    ];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class, 'project_id', 'id')->withTrashed();
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
