6f3b71161fd73a245ed6a78250e72d84fdfbd535
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-13 14:22:34 -0400

    feat: Add IssueCommented notification
    This commit adds a new notification class called `IssueCommented`. This notification is triggered when a new comment is added to an issue. It sends an email notification to the relevant users and also stores the notification in the database. The email notification includes the details of the issue and a link to view the issue. This notification improves the communication and collaboration among users in the application.

--- forge-app/app/Settings/GeneralSettings.php
+++ forge-app/app/Settings/GeneralSettings.php
@@ -7,14 +7,15 @@ use Spatie\LaravelSettings\Settings;
 class GeneralSettings extends Settings
 {
     public string $site_name;
-
     public bool $site_active;
+    public bool $enable_registration;
+    public string|null $site_description;
+    public string|null $site_logo;
+    public string|null $site_favicon;
+    public string|null $default_role;
+    public bool $enable_login_form;
+    public bool $enable_registration_form;
 
-    public string $site_description;
-
-    public string $site_logo;
-
-    public string $site_favicon;
 
     public static function group(): string
     {
