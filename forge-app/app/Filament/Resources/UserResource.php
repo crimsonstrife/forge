70f51a23df9b233ead893f28cc1834febe2fddaa
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-14 10:45:10 -0400

    feat: Add Filament resource page for viewing user details
    This commit adds a new Filament resource page for viewing user details. The `ViewUser` page extends the `ViewRecord` class from Filament and specifies the `UserResource` class. This addition enhances the user interface of the application by allowing users to view individual user records.

--- forge-app/app/Filament/Resources/UserResource.php
+++ forge-app/app/Filament/Resources/UserResource.php
@@ -10,20 +10,66 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Support\HtmlString;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\Pages\CreateRecord;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'fas fa-users';
+
+    protected static ?int $navigationSort = 1;
+
+    public static function getNavigationLabel(): string
+    {
+        return __('Users');
+    }
+
+    public static function getPluralLabel(): ?string
+    {
+        return static::getNavigationLabel();
+    }
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __('Permissions');
+    }
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\Grid::make()
+                            ->schema([
+                                Forms\Components\TextInput::make('name')
+                                    ->label(__('Full name'))
+                                    ->required()
+                                    ->maxLength(255),
+
+                                Forms\Components\TextInput::make('email')
+                                    ->label(__('Email address'))
+                                    ->email()
+                                    ->required()
+                                    ->rule(
+                                        fn($record) => 'unique:users,email,'
+                                            . ($record ? $record->id : 'NULL')
+                                            . ',id,deleted_at,NULL'
+                                    )
+                                    ->maxLength(255),
+
+                                Forms\Components\CheckboxList::make('roles')
+                                    ->label(__('Permission roles'))
+                                    ->required()
+                                    ->columns(3)
+                                    ->relationship('roles', 'name'),
+                            ]),
+                    ])
             ]);
     }
 
@@ -31,18 +77,45 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')
+                    ->label(__('Full name'))
+                    ->sortable()
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('email')
+                    ->label(__('Email address'))
+                    ->sortable()
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('roles.name')
+                    ->label(__('Roles'))
+                    ->limit(2),
+
+                Tables\Columns\TextColumn::make('email_verified_at')
+                    ->label(__('Email verified at'))
+                    ->dateTime()
+                    ->sortable()
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('socials')
+                    ->label(__('Linked social networks'))
+                    ->view('partials.filament.resources.social-icon'),
+
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label(__('Created at'))
+                    ->dateTime()
+                    ->sortable()
+                    ->searchable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 
@@ -58,6 +131,7 @@ class UserResource extends Resource
         return [
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
