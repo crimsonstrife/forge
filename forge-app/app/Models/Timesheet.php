e81d9ba334e07aa77e85248553518f9b40ca6c95
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-14 14:47:31 -0400

    feat: Add Timesheet model
    This commit adds the Timesheet model to the application. The Timesheet model represents a timesheet in the system and extends the base Model class. This addition allows for the management and tracking of timesheets within the application.

---
+++ forge-app/app/Models/Timesheet.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Timesheet Model
+ *
+ * This class represents the Timesheet model in the application.
+ * It extends the base Model class and uses the HasFactory trait.
+ */
+class Timesheet extends Model
+{
+    use HasFactory;
+}
