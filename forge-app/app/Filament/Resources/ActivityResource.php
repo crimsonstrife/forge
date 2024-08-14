cc77ef6e0a225dc1e24221ae5cd3fd3afb278bf8
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-08-14 11:14:31 -0400

    feat: Update ActivityResource with navigation label, icon, and priority
    This commit updates the `ActivityResource` class by adding a navigation label, icon, and priority. The navigation label is set to "Activities" and the icon is changed to "fas fa-clipboard-list". Additionally, the priority is set to 1. These changes enhance the user interface of the application by providing a clear label and icon for the activities section.

--- forge-app/app/Filament/Resources/ActivityResource.php
+++ forge-app/app/Filament/Resources/ActivityResource.php
@@ -17,13 +17,45 @@ class ActivityResource extends Resource
 {
     protected static ?string $model = Activity::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'fas fa-clipboard-list';
+
+    protected static ?int $priority = 1;
+
+    public static function getNavigationLabel(): string
+    {
+        return __('Activities');
+    }
+
+    public static function getPluralLabel(): ?string
+    {
+        return static::getNavigationLabel();
+    }
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __('Referential');
+    }
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Section::make()
+                    ->schema([
+                        Forms\Components\Grid::make()
+                            ->schema([
+                                Forms\Components\TextInput::make('name')
+                                    ->label(__('Activity name'))
+                                    ->required()
+                                    ->maxLength(255),
+
+                                Forms\Components\RichEditor::make('description')
+                                    ->label(__('Description'))
+                                    ->required()
+                                    ->columnSpan(2),
+
+                            ])
+                    ])
             ]);
     }
 
@@ -31,19 +63,28 @@ class ActivityResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')
+                    ->label(__('Activity name'))
+                    ->sortable()
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label(__('Created at'))
+                    ->dateTime()
+                    ->sortable()
+                    ->searchable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+                Tables\Actions\DeleteBulkAction::make(),
+            ])
+            ->defaultSort('id');
     }
 
     public static function getRelations(): array
@@ -58,6 +99,7 @@ class ActivityResource extends Resource
         return [
             'index' => Pages\ListActivities::route('/'),
             'create' => Pages\CreateActivity::route('/create'),
+            'view' => Pages\ViewActivity::route('/{record}'),
             'edit' => Pages\EditActivity::route('/{record}/edit'),
         ];
     }
