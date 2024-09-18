ffe49361b0c1c9755e6b9f698965db0beb557a46
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-09-18 13:46:17 -0400

    refactor: Update namespaces and references for PermissionSeeder and add special permissions
    This commit updates the namespaces and references for the PermissionSeeder class, ensuring consistency and proper usage throughout the application. Additionally, it adds special permissions such as 'access-filament', 'access-jetstream', 'access-horizon', 'access-telescope', 'manage-settings', 'view-analytics', 'is-admin', and 'is-super-admin'. These changes improve code organization and enhance the functionality of the PermissionSeeder.

--- forge-app/database/seeders/DatabaseSeeder.php
+++ forge-app/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+//use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -21,9 +21,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //User::factory()->create([
+        //    'name' => 'Test User',
+        //    'email' => 'test@example.com',
+        //]);
+
+        //Seed the permissions
+        $this->call(PermissionSeeder::class);
     }
 }
