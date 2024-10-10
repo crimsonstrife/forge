81997e7021231c4cb46b07ac2ec0f94b0b6963ff
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-10-10 16:10:24 -0400

    feat: add migrations for module settings and gitea settings

---
+++ forge-app/database/settings/2024_10_10_200657_gitea_settings.php
@@ -0,0 +1,34 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+return new class extends SettingsMigration
+{
+    /**
+     * Run the "up" method of the migration.
+     *
+     * This method adds module settings to the database.
+     * It sets the Gitea module to be disabled by default.
+     */
+    public function up(): void
+    {
+        $this->migrator->add('gitea.enabled', false);
+        $this->migrator->add('gitea.base_url', '');
+        $this->migrator->add('gitea.api_token', '');
+    }
+
+    /**
+     * Delete general settings from the database.
+     *
+     * This method is used to delete the following general settings from the database:
+     * - gitea.enabled
+     * - gitea.base_url
+     * - gitea.api_token
+     */
+    public function down(): void
+    {
+        $this->migrator->delete('gitea.enabled');
+        $this->migrator->delete('gitea.base_url');
+        $this->migrator->delete('gitea.api_token');
+    }
+};
