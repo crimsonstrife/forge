df462e584230644a545d0a8717f2b489bea3fe85
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-10-11 17:04:55 -0400

    Refactor ListDiscordConfig page to add create action for Discord Config but only when there is no record

--- forge-app/app/Filament/Resources/DiscordConfigResource/Pages/ListDiscordConfig.php
+++ forge-app/app/Filament/Resources/DiscordConfigResource/Pages/ListDiscordConfig.php
@@ -5,6 +5,10 @@ namespace App\Filament\Resources\DiscordConfigResource\Pages;
 use App\Filament\Resources\DiscordConfigResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Actions\Action;
+use App\Models\DiscordConfig;
+use App\Settings\DiscordSettings;
+use App\Settings\ModuleSettings;
 
 class ListDiscordConfig extends ListRecords
 {
@@ -12,8 +16,17 @@ class ListDiscordConfig extends ListRecords
 
     protected function getHeaderActions(): array
     {
+        // Check if the Discord module is enabled from the ModuleSettings
+        $isEnabled = app(ModuleSettings::class)->discord_enabled;
+
         return [
-            Actions\CreateAction::make(),
+            // Create a new Discord Config, only if one doesn't already exist. There should only be one Discord Config in the database.
+            Actions\CreateAction::make()
+                ->label('Create Discord Config')
+                ->modal('create-discord-config')
+                ->icon('heroicon-o-plus-circle')
+                ->button()
+                ->visible(fn () => DiscordConfig::count() === 0 && $isEnabled),
         ];
     }
 }
