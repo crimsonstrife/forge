513c333a2926260d5d58dc1d0a529bbad2c720c7
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-10-14 10:29:04 -0400

    Refactor DiscordSettings to use DiscordConfig model for default values

--- forge-app/app/Settings/DiscordSettings.php
+++ forge-app/app/Settings/DiscordSettings.php
@@ -2,6 +2,7 @@
 
 namespace App\Settings;
 
+use App\Models\DiscordConfig;
 use Spatie\LaravelSettings\Settings;
 
 /**
@@ -31,6 +32,26 @@ class DiscordSettings extends Settings
     }
 
     /**
+     * Define the default values using the defaults() method.
+     */
+    public static function defaults(): array
+    {
+        // Get the DiscordConfig from the database
+        $discordConfig = DiscordConfig::first();
+
+        return [
+            'enabled' => false,
+            'client_id' => $discordConfig->client_id ?? env('DISCORD_CLIENT_ID', ''),
+            'client_secret' => $discordConfig->client_secret ?? env('DISCORD_CLIENT_SECRET', ''),
+            'bot_token' => $discordConfig->bot_token ?? env('DISCORD_BOT_TOKEN', ''),
+            'guild_id' => $discordConfig->guild_id ?? env('DISCORD_GUILD_ID', ''),
+            'redirect_uri' => $discordConfig->redirect_uri ?? env('DISCORD_REDIRECT_URI', ''),
+            'role_mappings' => $discordConfig->role_mappings ?? [],
+            'channel_mappings' => $discordConfig->channel_mappings ?? [],
+        ];
+    }
+
+    /**
      * Creates a new instance of DiscordSettings with default values.
      *
      * @return DiscordSettings The newly created instance.
@@ -39,6 +60,13 @@ class DiscordSettings extends Settings
     {
         return new self([
             'enabled' => false,
+            'client_id' => '',
+            'client_secret' => '',
+            'bot_token' => '',
+            'guild_id' => '',
+            'redirect_uri' => '',
+            'role_mappings' => [],
+            'channel_mappings' => [],
         ]);
     }
 
@@ -61,157 +89,6 @@ class DiscordSettings extends Settings
     public function setEnabled(bool $enabled): void
     {
         $this->enabled = $enabled;
-    }
-
-    /**
-     * Set the client ID for Discord OAuth.
-     *
-     * @param string $client_id The client ID to set.
-     * @return void
-     */
-    public function setClientId(string $client_id): void
-    {
-        $this->client_id = $client_id;
-    }
-
-    /**
-     * Set the client secret for Discord OAuth.
-     * Should be kept secret, encrypted
-     *
-     * @param string $client_secret The client secret to set.
-     * @return void
-     */
-    public function setClientSecret(string $client_secret): void
-    {
-        $this->attributes['client_secret'] = encrypt($client_secret);
-    }
-
-    /**
-     * Set the bot token for Discord.
-     * Should be kept secret, encrypted
-     *
-     * @param string $bot_token The bot token to set.
-     * @return void
-     */
-    public function setBotToken(string $bot_token): void
-    {
-        $this->attributes['bot_token'] = encrypt($bot_token);
-    }
-
-    /**
-     * Set the guild ID for Discord.
-     *
-     * @param string $guild_id The guild ID to set.
-     * @return void
-     */
-    public function setGuildId(string $guild_id): void
-    {
-        $this->guild_id = $guild_id;
-    }
-
-    /**
-     * Set the redirect URI for Discord OAuth.
-     *
-     * @param string $redirect_uri The redirect URI to set.
-     * @return void
-     */
-    public function setRedirectUri(string $redirect_uri): void
-    {
-        $this->redirect_uri = $redirect_uri;
-    }
-
-    /**
-     * Set the role mappings for Discord.
-     *
-     * @param array $role_mappings The role mappings to set.
-     * @return void
-     */
-    public function setRoleMappings(array $role_mappings): void
-    {
-        $this->role_mappings = $role_mappings;
-    }
-
-    /**
-     * Set the channel mappings for Discord.
-     *
-     * @param array $channel_mappings The channel mappings to set.
-     * @return void
-     */
-    public function setChannelMappings(array $channel_mappings): void
-    {
-        $this->channel_mappings = $channel_mappings;
-    }
-
-    /**
-     * Retrieves the client ID for Discord OAuth.
-     *
-     * @return string The client ID.
-     */
-    public function getClientId(): string
-    {
-        return $this->client_id;
-    }
-
-    /**
-     * Retrieves the client secret for Discord OAuth.
-     *
-     * @return string The client secret.
-     */
-    public function getClientSecret(): string
-    {
-        // Decrypt the client secret
-        return decrypt($this->client_secret);
-    }
-
-    /**
-     * Retrieves the bot token for Discord.
-     *
-     * @return string The bot token.
-     */
-    public function getBotToken(): string
-    {
-        // Decrypt the bot token
-        return decrypt($this->bot_token);
-    }
-
-    /**
-     * Retrieves the guild ID for Discord.
-     *
-     * @return string The guild ID.
-     */
-    public function getGuildId(): string
-    {
-        return $this->guild_id;
-    }
-
-    /**
-     * Retrieves the redirect URI for Discord OAuth.
-     *
-     * @return string The redirect URI.
-     */
-    public function getRedirectUri(): string
-    {
-        return $this->redirect_uri;
-    }
-
-    /**
-     * Retrieves the role mappings for Discord.
-     *
-     * @return array The role mappings.
-     */
-    public function getRoleMappings(): array
-    {
-        return $this->role_mappings;
-    }
-
-    /**
-     * Retrieves the channel mappings for Discord.
-     *
-     * @return array The channel mappings.
-     */
-    public function getChannelMappings(): array
-    {
-        return $this->channel_mappings;
     }
 
     /**
