e2901e06dc3b1f18590fb959b8348f057112d14c
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-10-25 08:43:54 -0400

    Refactor file paths for user-uploaded icons and bots

--- forge-app/config/filesystems.php
+++ forge-app/config/filesystems.php
@@ -71,7 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('bots') => storage_path('app/public/bots'),
+        public_path('bots') => storage_path('app/bots'),
     ],
 
 ];
