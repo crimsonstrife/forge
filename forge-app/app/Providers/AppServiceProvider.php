58e333b3c48b7b39df0ae41f36d2faefa67b8cd1
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-10-28 23:30:55 -0400

    Update package-lock.json and composer.json

--- forge-app/app/Providers/AppServiceProvider.php
+++ forge-app/app/Providers/AppServiceProvider.php
@@ -53,22 +53,26 @@ class AppServiceProvider extends ServiceProvider
 
     private function configureApp(): void
     {
-        try {
-            $settings = app(GeneralSettings::class);
-            Config::set('app.locale', $settings->site_language ?? config('app.fallback_locale'));
-            Config::set('app.name', $settings->site_name ?? env('APP_NAME'));
-            Config::set('filament.brand', $settings->site_name ?? env('APP_NAME'));
-            Config::set(
-                'app.logo',
-                $settings->site_logo ? asset('storage/' . $settings->site_logo) : asset('favicon.ico')
-            );
-            Config::set('filament-breezy.enable_registration', $settings->enable_registration ?? false);
-            Config::set('filament-socialite.registration', $settings->enable_registration ?? false);
-            Config::set('filament-socialite.enabled', $settings->enable_social_login ?? false);
-            Config::set('system.login_form.is_enabled', $settings->enable_login_form ?? false);
-            Config::set('services.oidc.is_enabled', $settings->enable_oidc_login ?? false);
-        } catch (QueryException $e) {
-            // Error: No database configured yet
-        }
+        // Check if the database exists, and if so, do any of the GeneralSettings exist in the settings table?
+        // If not, do not attempt to load the settings or Spatie will throw an error.
+
+
+        // try {
+        //     $settings = app(GeneralSettings::class);
+        //     Config::set('app.locale', $settings->site_language ?? config('app.fallback_locale'));
+        //     Config::set('app.name', $settings->site_name ?? env('APP_NAME'));
+        //     Config::set('filament.brand', $settings->site_name ?? env('APP_NAME'));
+        //     Config::set(
+        //         'app.logo',
+        //         $settings->site_logo ? asset('storage/' . $settings->site_logo) : asset('favicon.ico')
+        //     );
+        //     Config::set('filament-breezy.enable_registration', $settings->enable_registration ?? false);
+        //     Config::set('filament-socialite.registration', $settings->enable_registration ?? false);
+        //     Config::set('filament-socialite.enabled', $settings->enable_social_login ?? false);
+        //     Config::set('system.login_form.is_enabled', $settings->enable_login_form ?? false);
+        //     Config::set('services.oidc.is_enabled', $settings->enable_oidc_login ?? false);
+        // } catch (QueryException $e) {
+        //     // Error: No database configured yet
+        // }
     }
 }
