26c09c55dd1ae28aa5af3ae35df41738743ea17a
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-10-29 15:33:21 -0400

    Refactor AppServiceProvider to handle GeneralSettings and configure app

--- forge-app/app/Providers/AppServiceProvider.php
+++ forge-app/app/Providers/AppServiceProvider.php
@@ -53,41 +53,24 @@ class AppServiceProvider extends ServiceProvider
 
     private function configureApp(): void
     {
-        // Check if the database exists, and if so, do any of the GeneralSettings exist in the settings table?
-        // If not, do not attempt to load the settings or Spatie will throw an error.
-        $dbExists = false;
         try {
-            $dbExists = $this->app->make('db')->connection()->DB::table('settings')->exists(); // Check if the settings table exists
-
-            // If the settings table exists, check if any of the GeneralSettings exist before loading them
-            if ($dbExists) {
-                try {
-                    $settings = app(GeneralSettings::class);
-                    Config::set('app.locale', $settings->site_language ?? config('app.fallback_locale'));
-                    Config::set('app.name', $settings->site_name ?? env('APP_NAME'));
-                    Config::set('filament.brand', $settings->site_name ?? env('APP_NAME'));
-                    Config::set(
-                        'app.logo',
-                        $settings->site_logo ? asset('storage/' . $settings->site_logo) : asset('favicon.ico')
-                    );
-                    Config::set('filament-breezy.enable_registration', $settings->enable_registration ?? false);
-                    Config::set('filament-socialite.registration', $settings->enable_registration ?? false);
-                    Config::set('filament-socialite.enabled', $settings->enable_social_login ?? false);
-                    Config::set('system.login_form.is_enabled', $settings->enable_login_form ?? false);
-                    Config::set('services.oidc.is_enabled', $settings->enable_oidc_login ?? false);
-                } catch (QueryException $e) {
-                    // If the environment is local, throw a warning but do nothing, otherwise, throw an error
-                    if ($this->app->environment('local')) {
-                        $this->app->make('log')->warning('GeneralSettings not found in the settings table.');
-                    } else {
-                        throw $e;
-                    }
-                }
-            }
-        } catch (\Exception $e) {
+            $settings = app(GeneralSettings::class);
+            Config::set('app.locale', $settings->site_language ?? config('app.fallback_locale'));
+            Config::set('app.name', $settings->site_name ?? env('APP_NAME'));
+            Config::set('filament.brand', $settings->site_name ?? env('APP_NAME'));
+            Config::set(
+                'app.logo',
+                $settings->site_logo ? asset('storage/' . $settings->site_logo) : asset('favicon.ico')
+            );
+            Config::set('filament-breezy.enable_registration', $settings->enable_registration ?? false);
+            Config::set('filament-socialite.registration', $settings->enable_registration ?? false);
+            Config::set('filament-socialite.enabled', $settings->enable_social_login ?? false);
+            Config::set('system.login_form.is_enabled', $settings->enable_login_form ?? false);
+            Config::set('services.oidc.is_enabled', $settings->enable_oidc_login ?? false);
+        } catch (QueryException $e) {
             // If the environment is local, throw a warning but do nothing, otherwise, throw an error
             if ($this->app->environment('local')) {
-                $this->app->make('log')->warning('Database not found or settings table not found.');
+                $this->app->make('log')->warning('GeneralSettings not found in the settings table.');
             } else {
                 throw $e;
             }
