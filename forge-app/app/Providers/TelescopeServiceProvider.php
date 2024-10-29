523dc7764a77a26f4ca445bbd0cf606e3b4f1fc9
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2024-10-29 17:03:16 -0400

    Refactor TelescopeServiceProvider to import Illuminate\Support\Collection

--- forge-app/app/Providers/TelescopeServiceProvider.php
+++ forge-app/app/Providers/TelescopeServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
+use Illuminate\Support\Collection;
 
 /**
  * TelescopeServiceProvider class.
