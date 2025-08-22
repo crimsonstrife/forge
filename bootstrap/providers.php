bc3e0a30b00cb759e7323a1aab4d670191e828e2
Patrick Barnhardt <contact@patrickbarnhardt.info>
2025-08-22 14:29:03 -0400

    Add broadcasting configuration and integrate Pulse and Telescope services

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,10 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\FolioServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
