a9d3b8e18dd84ac787ed03adba577084bb0e342f
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2025-08-25 22:20:07 -0400

    Add 'color' column to issue_statuses table migration
    This change introduces a new 'color' column to the issue_statuses table to support storing color codes for issue statuses. It enhances the flexibility in categorizing and visually differentiating statuses.

--- database/migrations/2025_08_25_021334_create_issue_statuses_table.php
+++ database/migrations/2025_08_25_021334_create_issue_statuses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('key')->unique();
+            $table->string('color');
             $table->unsignedInteger('order')->default(0)->index();
             $table->boolean('is_done')->default(false);
             $table->timestamps();
